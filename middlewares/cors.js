663c7a40850f4c3240bba6897374d9e1550c737e
TheSpiffyOne <egor.prelovskiy@mail.ru>
2024-05-21 00:47:28 +0300

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,19 +1,16 @@
-const allowedCors = [
-    'https://practicum.yandex.ru',
-    'https://students-projects.ru',
-    'localhost:3000',
-    'localhost:3001'
-];
+// Вместо нашего адреса вставь свой
+// Можно ещё не удалять адреса с localhost, но мы удалили для краткости
+const allowedCors = ["https://pindie-frontend-spiffy.nomoredomainswork.ru"];
 
 function cors(req, res, next) {
-    const { origin } = req.headers;
-    if (allowedCors.includes(origin)) {
-        res.header('Access-Control-Allow-Origin', origin);
-    }
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  const { origin } = req.headers;
 
-    next();
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  next();
 }
 
 module.exports = cors;
