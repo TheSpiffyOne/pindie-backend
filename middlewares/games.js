26dd6a64314d52efc7785d3db8d355ebdb8ff4c9
TheSpiffyOne <egor.prelovskiy@mail.ru>
2024-05-25 17:46:17 +0300

    1.0
    yes

---
+++ middlewares/games.js
@@ -0,0 +1,71 @@
+const games = require("../models/game")
+
+const createGame = async (req, res, next) => {
+  console.log("POST /games");
+  try {
+    console.log(req.body);
+    req.game = await games.create(req.body);
+    next();
+  } catch (error) {
+    res.status(400).send("Error creating game");
+  }
+};
+const findGameById = async (req, res, next) => {
+  try {
+    req.game = await games
+      .findById(req.params.id)
+      .populate("categories")
+      .populate({path: "users", select: "-password"});
+    next();
+  } catch (error) {
+    res.status(404).send({ message: "Игра не найдена" });
+  }
+};
+
+const findAllGames = async (req, res, next) => {
+  // Поиск всех игр в проекте по заданной категории
+  if(req.query["categories.name"]) { 
+    req.gamesArray = await games.findGameByCategory(req.query["categories.name"]);
+    next();
+    return;
+  }
+  // Поиск всех игр в проекте
+  req.gamesArray = await games
+    .find({})
+    .populate("categories")
+    .populate({
+      path: "users",
+      select: "-password" // Исключим данные о паролях пользователей
+    })
+  next();
+};
+
+
+const updateGame = async (req, res, next) => {
+  try {
+    req.game = await games.findByIdAndUpdate(req.params.id, req.body);
+    next();
+  } catch (error) {
+    res.status(400).send({ message: "Ошибка обновления игры" });
+  }
+};
+
+const deleteGame = async (req, res, next) => {
+  try {
+    req.game = await games.findByIdAndDelete(req.params.id);
+    next();
+  } catch (error) {
+    res.status(400).send({ message: "Error deleting game" });
+  }
+};
+
+const checkIsVoteRequest = async (req, res, next) => {
+  // Если в запросе присылают только поле users
+if (Object.keys(req.body).length === 1 && req.body.users) {
+  req.isVoteRequest = true;
+}
+next();
+};
+
+module.exports = { createGame, findGameById, findAllGames, updateGame, deleteGame, checkIsVoteRequest }
+
---
+++ middlewares/users.js
@@ -0,0 +1,81 @@
+const users = require("../models/user");
+const bcrypt = require("bcryptjs");
+
+const findAllUsers = async (req, res, next) => {
+  req.usersArray = await users.find({});
+  next();
+}
+const createUser = async (req, res, next) => {
+  try {
+    req.user = await users.create(req.body);
+    next();
+  } catch (error) {
+    res.status(400).send("Ошибка при создании пользователя");
+  }
+};
+
+const findUserById = async (req, res, next) => {
+  console.log("GET /users/:id");
+  try {
+    req.user = await users.findById(req.params.id);
+    next();
+  } catch (error) {
+    res.setHeader("Content-Type", "application/json");
+    res.status(404).send(JSON.stringify({ message: "Пользователь не найдена" }));
+  }
+};
+
+const deleteUser = async (req, res, next) => {
+  console.log("DELETE /users/:id");
+  try {
+    req.user = await users.findByIdAndDelete(req.params.id);
+    next();
+  } catch (error) {
+    res.setHeader("Content-Type", "application/json");
+    res.status(400).send(JSON.stringify({ message: "Ошибка удаления пользователя" }));
+  }
+};
+
+const filterPassword = (req, res, next) => {
+  const filterUser = (user) => {
+    const { password, ...userWithoutPassword } = user.toObject();
+    return userWithoutPassword;
+  }; if (req.user) {
+    req.user = filterUser(req.user);
+  }
+  if (req.usersArray) {
+    req.usersArray = req.usersArray.map((user) => filterUser(user));
+  }
+    next();
+};
+
+const updateUser = async (req, res, next) => {
+  console.log(`PUT /users/${req.params.id}`)
+  try {
+    if (req.body.name || req.body.email || req.body.password) {
+      req.user = await users.findByIdAndUpdate(req.params.id, req.body);
+      next();
+    } else {
+      res.status(400).send(JSON.stringify({ message: "Ошибка обновления пользователя" }));
+    }
+  } catch (error) {
+    res.setHeader("Content-Type", "application/json");
+    res.status(400).send(JSON.stringify({ message: "ошибка обновления пользователя" }));
+  }
+};
+
+const hashPassword = async (req, res, next) => {
+  try {
+    // Создаём случайную строку длиной в десять символов
+    const salt = await bcrypt.genSalt(10);
+    // Хешируем пароль
+    const hash = await bcrypt.hash(req.body.password, salt);
+    // Полученный в запросе пароль подменяем на хеш
+    req.body.password = hash;
+    next();
+  } catch (error) {
+    res.status(400).send({ message: "Ошибка хеширования пароля" });
+  }
+};
+
+module.exports = { findAllUsers, createUser, findUserById, deleteUser, updateUser, filterPassword, hashPassword }
